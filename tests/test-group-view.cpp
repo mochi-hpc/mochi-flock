7975cbaede6281da4495b0074f66cbd4f1a45441
Matthieu Dorier <mdorier@anl.gov>
2024-03-28 09:50:02 +0000

    added test for group views

---
+++ tests/test-group-view.cpp
@@ -0,0 +1,180 @@
+/*
+ * (C) 2024 The University of Chicago
+ *
+ * See COPYRIGHT in top-level directory.
+ */
+#include <stdio.h>
+#include <margo.h>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_all.hpp>
+#include <flock/flock-group-view.h>
+#include <unordered_map>
+
+struct TestContext {
+
+    margo_instance_id mid = MARGO_INSTANCE_NULL;
+    hg_addr_t         addr = HG_ADDR_NULL;
+
+    TestContext() {
+        mid = margo_init("na+sm", MARGO_SERVER_MODE, 0, 0);
+        margo_addr_self(mid, &addr);
+    }
+
+    ~TestContext() {
+        margo_addr_free(mid, addr);
+        margo_finalize(mid);
+    }
+};
+
+struct Member {
+    uint64_t    rank;
+    std::string address;
+    uint16_t    provider_id;
+};
+
+TEST_CASE("Test group view interface", "[group-view]") {
+
+    // create test context
+    auto context = std::make_unique<TestContext>();
+    bool b;
+
+    SECTION("Create group view") {
+
+        flock_group_view_t view = FLOCK_GROUP_VIEW_INITIALIZER;
+        REQUIRE(view.members.size == 0);
+        REQUIRE(view.members.capacity == 0);
+        REQUIRE(view.members.data == nullptr);
+        REQUIRE(view.metadata.size == 0);
+        REQUIRE(view.metadata.capacity == 0);
+        REQUIRE(view.metadata.data == nullptr);
+
+        std::vector<Member> members_ref(16);
+        for(size_t i = 0; i < 16; ++i) {
+            char address[64];
+            sprintf(address, "address/%02lu", i);
+            uint64_t rank = (i*3)%16;
+            uint16_t provider_id = i+42;
+            b = flock_group_view_add_member(
+                    &view, rank, provider_id, address);
+            REQUIRE(b);
+            members_ref[rank] = {rank, address, provider_id};
+        }
+
+        std::unordered_map<std::string, std::string> metadata_ref;
+        for(size_t i = 0; i < 8; ++i) {
+            char key[64];
+            char value[64];
+            sprintf(key+1, "_key%lu", i);
+            key[0] = 'A' + ((i*3)%8);
+            sprintf(value, "value_%lu", i);
+            b = flock_group_view_add_metadata(&view, key, value);
+            REQUIRE(b);
+            metadata_ref.insert({key,value});
+        }
+
+        REQUIRE(view.members.size == 16);
+        REQUIRE(view.members.capacity >= 16);
+        REQUIRE(view.members.data != nullptr);
+        REQUIRE(view.metadata.size == 8);
+        REQUIRE(view.metadata.capacity >= 8);
+        REQUIRE(view.metadata.data != nullptr);
+
+        for(size_t i = 0; i < view.members.size; ++i) {
+            const flock_member_t* member = flock_group_view_find_member(&view, i);
+            REQUIRE(members_ref[i].address == member->address);
+            REQUIRE(members_ref[i].provider_id == member->provider_id);
+            REQUIRE(members_ref[i].rank == member->rank);
+            REQUIRE(i == member->rank);
+        }
+
+        const flock_member_t* member = flock_group_view_find_member(&view, view.members.size);
+        REQUIRE(member == nullptr);
+
+        for(auto& p : metadata_ref) {
+            const char* value = flock_group_view_find_metadata(&view, p.first.c_str());
+            REQUIRE(value != nullptr);
+            REQUIRE(p.second == value);
+        }
+
+        const char* value = flock_group_view_find_metadata(&view, "abcd");
+        REQUIRE(value == nullptr);
+
+        // Try to remove a rank that does not exist
+        b = flock_group_view_remove_member(&view, view.members.size);
+        REQUIRE(!b);
+
+        // Remove rank 5
+        b = flock_group_view_remove_member(&view, 5);
+        REQUIRE(b);
+
+        REQUIRE(view.members.size == 15);
+
+        for(size_t i = 0; i < members_ref.size(); ++i) {
+            const flock_member_t* member = flock_group_view_find_member(&view, i);
+            if(i == 5) {
+                REQUIRE(member == nullptr);
+                continue;
+            }
+            REQUIRE(members_ref[i].address == member->address);
+            REQUIRE(members_ref[i].provider_id == member->provider_id);
+            REQUIRE(members_ref[i].rank == member->rank);
+            REQUIRE(i == member->rank);
+        }
+
+        // Try to remove a metadata that does not exist
+        b = flock_group_view_remove_metadata(&view, "abcd");
+        REQUIRE(!b);
+
+        // Remove a metadata that does exist
+        auto key_to_remove = metadata_ref.begin()->first;
+        b = flock_group_view_remove_metadata(&view, key_to_remove.c_str());
+        REQUIRE(b);
+
+        for(auto& p : metadata_ref) {
+            const char* value = flock_group_view_find_metadata(&view, p.first.c_str());
+            if(p.first == key_to_remove) {
+                REQUIRE(value == nullptr);
+                continue;
+            }
+            REQUIRE(value != nullptr);
+            REQUIRE(p.second == value);
+        }
+
+        auto members_data = view.members.data;
+        auto members_size = view.members.size;
+        auto members_capa = view.members.capacity;
+
+        auto metadata_data = view.metadata.data;
+        auto metadata_size = view.metadata.size;
+        auto metadata_capa = view.metadata.capacity;
+
+        // Move the view to another destination
+        flock_group_view_t view2 = FLOCK_GROUP_VIEW_INITIALIZER;
+        FLOCK_GROUP_VIEW_MOVE(&view, &view2);
+
+        REQUIRE(view.members.size == 0);
+        REQUIRE(view.members.capacity == 0);
+        REQUIRE(view.members.data == nullptr);
+        REQUIRE(view.metadata.size == 0);
+        REQUIRE(view.metadata.capacity == 0);
+        REQUIRE(view.metadata.data == nullptr);
+
+        REQUIRE(view2.members.size == members_size);
+        REQUIRE(view2.members.capacity == members_capa);
+        REQUIRE(view2.members.data == members_data);
+        REQUIRE(view2.metadata.size == metadata_size);
+        REQUIRE(view2.metadata.capacity == metadata_capa);
+        REQUIRE(view2.metadata.data == metadata_data);
+
+        // Clear the view2
+        flock_group_view_clear(&view);
+        flock_group_view_clear(&view2);
+
+        REQUIRE(view2.members.size == 0);
+        REQUIRE(view2.members.capacity == 0);
+        REQUIRE(view2.members.data == nullptr);
+        REQUIRE(view2.metadata.size == 0);
+        REQUIRE(view2.metadata.capacity == 0);
+        REQUIRE(view2.metadata.data == nullptr);
+    }
+}
