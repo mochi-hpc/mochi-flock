d5fbebfdd38531a6e3910588b89cec9c0548e352
Matthieu Dorier <mdorier@anl.gov>
2024-06-06 09:38:24 +0100

    updated flock-common.h

--- include/flock/flock-common.h
+++ include/flock/flock-common.h
@@ -23,28 +23,34 @@ typedef enum flock_update_t {
 #define FLOCK_MODE_INIT_UPDATE 0x1 /* Update the group on initialization */
 #define FLOCK_MODE_SUBSCRIBE   0x2 /* Subscribe to the group on initialization */
 
+#define FLOCK_RETURN_VALUES                              \
+    X(FLOCK_SUCCESS, "Success")                          \
+    X(FLOCK_ERR_ALLOCATION, "Allocation error")          \
+    X(FLOCK_ERR_INVALID_ARGS, "Invalid argument")        \
+    X(FLOCK_ERR_INVALID_PROVIDER, "Invalid provider id") \
+    X(FLOCK_ERR_INVALID_GROUP, "Invalid group id")       \
+    X(FLOCK_ERR_INVALID_BACKEND, "Invalid backend type") \
+    X(FLOCK_ERR_INVALID_CONFIG, "Invalid configuration") \
+    X(FLOCK_ERR_FROM_MERCURY, "Mercurt error")           \
+    X(FLOCK_ERR_FROM_ARGOBOTS, "Argobots error")         \
+    X(FLOCK_ERR_FROM_MPI, "MPI error")                   \
+    X(FLOCK_ERR_OP_UNSUPPORTED, "Unsupported operation") \
+    X(FLOCK_ERR_OP_FORBIDDEN, "Forbidden operation")     \
+    X(FLOCK_ERR_NO_MEMBER, "No member at this rank")     \
+    X(FLOCK_ERR_NO_METADATA, "Invalid metadata key")     \
+    X(FLOCK_ERR_NOT_A_MEMBER, "Process is not member")   \
+    X(FLOCK_ERR_RANK_USED, "Rank alread used")           \
+    X(FLOCK_ERR_OTHER, "Other error")
+
+
 /**
  * @brief Error codes that can be returned by FLOCK functions.
  */
+#define X(__err__, __msg__) __err__,
 typedef enum flock_return_t {
-    FLOCK_SUCCESS,
-    FLOCK_ERR_ALLOCATION,        /* Allocation error */
-    FLOCK_ERR_INVALID_ARGS,      /* Invalid argument */
-    FLOCK_ERR_INVALID_PROVIDER,  /* Invalid provider id */
-    FLOCK_ERR_INVALID_GROUP,     /* Invalid group id */
-    FLOCK_ERR_INVALID_BACKEND,   /* Invalid backend type */
-    FLOCK_ERR_INVALID_CONFIG,    /* Invalid configuration */
-    FLOCK_ERR_FROM_MERCURY,      /* Mercurt error */
-    FLOCK_ERR_FROM_ARGOBOTS,     /* Argobots error */
-    FLOCK_ERR_FROM_MPI,          /* MPI error */
-    FLOCK_ERR_OP_UNSUPPORTED,    /* Unsupported operation */
-    FLOCK_ERR_OP_FORBIDDEN,      /* Forbidden operation */
-    FLOCK_ERR_NO_MEMBER,         /* No member at this rank */
-    FLOCK_ERR_NO_METADATA,       /* Invalid metadata key */
-    FLOCK_ERR_NOT_A_MEMBER,      /* Process is not member of the group */
-    /* ... TODO add more error codes here if needed */
-    FLOCK_ERR_OTHER              /* Other error */
+    FLOCK_RETURN_VALUES
 } flock_return_t;
+#undef X
 
 /**
  * @brief Type of function called when a member joins, leaves, or dies.
