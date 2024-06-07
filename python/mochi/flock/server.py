fc55d5a99732dbf5403f900dc69eee7cf619da6d
Matthieu Dorier <mdorier@anl.gov>
2024-06-07 16:25:32 +0100

    added better python APIs

---
+++ python/mochi/flock/server.py
@@ -0,0 +1,26 @@
+# (C) 2024 The University of Chicago
+# See COPYRIGHT in top-level directory.
+
+
+"""
+.. module:: server
+   :synopsis: This package provides access to the Flock C++ wrapper
+
+.. moduleauthor:: Matthieu Dorier <mdorier@anl.gov>
+
+
+"""
+
+
+import pyflock_common
+from .view import GroupView
+import pyflock_server
+import pymargo.core
+import pymargo
+
+
+class Provider:
+
+    def __init__(self, engine: pymargo.core.Engine, provider_id: int, config: str, initial_view: GroupView):
+        self._internal = pyflock_server.Provider(
+            self._engine.get_internal_mid(), provider_id, config, initial_view)
