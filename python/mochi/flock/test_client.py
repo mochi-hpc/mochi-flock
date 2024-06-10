a89ce9ed5636829ff15d67027840c25d51dd1806
Matthieu Dorier <mdorier@anl.gov>
2024-06-10 09:11:27 +0100

    started adding unit tests for python module

---
+++ python/mochi/flock/test_client.py
@@ -0,0 +1,17 @@
+import unittest
+import mochi.flock.client as mfc
+from pymargo.core import Engine
+
+class TestClient(unittest.TestCase):
+
+    def test_init_client_from_address(self):
+        client = mfc.Client("na+sm")
+
+    def test_init_client_from_engine(self):
+        with Engine("na+sm") as engine:
+            client = mfc.Client(engine)
+            del client
+
+
+if __name__ == '__main__':
+    unittest.main()
